9b9940f0598a185a7ed55b828874fe3ae4fca879
adr1enette <matvejevs.piano@gmail.com>
2024-09-07 09:22:15 +0300

    Update Program.cs
    Added shuffle feature

--- GeniusIdiotConsoleApp/Program.cs
+++ GeniusIdiotConsoleApp/Program.cs
@@ -7,9 +7,10 @@ internal class Program
     private static void Main(string[] args)
     {
         const int questionCount = 5;
+        int[] indexArray = GenerateShuffledIndexArray(questionCount);
 
-        string[] questions = GetQuestions(questionCount);
-        int[] answers = GetAnswers(questionCount);
+        string[] questions = GetQuestions(indexArray);
+        int[] answers = GetAnswers(indexArray);
         string[] diagnoses = GetDiagnoses();
 
         int validAnswersCount = 0;
@@ -52,27 +53,56 @@ internal class Program
         return diagnoses;
     }
 
-    private static int[] GetAnswers(int questionCount)
+    private static T[] GetShuffledArray<T>(T[] array, int[] indexArray)
     {
-        int[] answers = new int[questionCount];
+        var shuffledArray = new T[indexArray.Length];
+        for (int i = 0; i < indexArray.Length; i++)
+        {
+            shuffledArray[i] = array[indexArray[i]];
+        }
+
+        return shuffledArray;
+    }
+
+    private static int[] GetAnswers(int[] indexArray)
+    {
+        int[] answers = new int[indexArray.Length];
         answers[0] = 6;
         answers[1] = 9;
         answers[2] = 25;
         answers[3] = 60;
         answers[4] = 2;
 
-        return answers;
+        return GetShuffledArray(answers, indexArray);
     }
 
-    private static string[] GetQuestions(int questionCount)
+    private static string[] GetQuestions(int[] indexArray)
     {
-        string[] questions = new string[questionCount];
+        string[] questions = new string[indexArray.Length];
         questions[0] = "Сколько будет два плюс два умноженное на два?";
         questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
         questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
         questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
         questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
 
-        return questions;
+        return GetShuffledArray(questions, indexArray);
+    }
+
+    private static int[] GenerateShuffledIndexArray(int questionCount)
+    {
+        int[] array = new int[questionCount];
+        for (int i = 0; i < questionCount; i++)
+        {
+            array[i] = i;
+        }
+
+        var random = new Random();
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+
+        return array;
     }
 }
